798016579ffb4db70ea944cd5197826670ea8bf9
Aakash Japi <aakashjapi@gmail.com>
2015-10-23 10:56:54 -0700

    markov bot p1 implemented; fun with iterators and all

--- markov.js
+++ markov.js
@@ -1,9 +1,28 @@
 
 var MarkovBot = function (text) {
 
-	this.cache = {}
-	this.txt = txt.replace(/[,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-	this.words = txt.split(" ")
-	
+	this.cache = {};
+	this.txt = text.replace(/[,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	this.words = this.txt.split(" ");
 
 }
+
+MarkovBot.prototype.triples = function () {
+	var w = 0;
+	var words = this.words;
+
+	if (this.words.length < 3) {
+		return;
+	} 
+	else {
+		return {
+			next : function () {
+				return w < words.length - 3 ?
+						{triple: [words[w], words[w+1], words[w+2]], ind: w++, done: false} :
+						{done: true}
+			}
+		}
+	}
+}
+
+
