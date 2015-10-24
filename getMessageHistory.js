5c0c8c810b7a57806ddbce9691cdb90f3c529fa6
Aakash Japi <aakashjapi@gmail.com>
2015-10-23 18:30:56 -0700

    doesn't work

--- getMessageHistory.js
+++ getMessageHistory.js
@@ -1,18 +1,32 @@
 
+function func(data) {
+	console.log(data);
+	alert(data);
+}
+
+
 function getMessageHistory () {
-	var func = function (data) {
-		console.log(data);
-		alert(data);
-	}
-	chrome.cookie = document.cookie;
+
+	//document.cookie = chrome.cookie;
 	$.ajax({
 		method: "POST",
+		cache: false,
 		url: "https://www.messenger.com/ajax/mercury/thread_info.php?__pc=EXP1%3Amessengerdotcom_pkg",
+		// headers:{
+  // //           'origin': 'https://www.messenger.com'
+  //    	},
 		dataType: "json",
+		setCookie: document.cookie,
 		success: func, 
-		jsonp: true,
-		jsonpCallback: func,
-		crossDomain: true
+		jsonp: false,
+		crossDomain: true,
+		async: true,
+		xhrFields: {
+			withCredentials: true
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+        	console.log(textStatus, errorThrown);
+    	}
 	});
 }
 
@@ -26,7 +40,7 @@ var chat_box = document.querySelector("[name=message_body]");
 // TODO: this selector breaks when there's multiple buttons (like if there's a popup box)
 var send_button = document.querySelector("#u_0_r");
 function send_message(message, delay) {
-  chat_box.classList.remove("DOMControl_placeholder");
+  #chat_box.classList.remove("DOMControl_placeholder");
   chat_box.value = message;
 
   // make Facebook create thumbnails for images
