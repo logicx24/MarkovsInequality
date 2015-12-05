95861e801a3c38d80f8c34e86e211b02d0a1cdc8
Aakash Japi <aakashjapi@gmail.com>
2015-12-04 18:17:14 -0800

    better mentions handling

--- functions/mention.js
+++ functions/mention.js
@@ -1,7 +1,7 @@
 module.exports.matchPattern = /@/;
-//@name lastname must be the ONLY thing in the message. I'll make a better regex soon.
+//@name lastname must be how you mention someone. No multiple mentions in a single comment (yet)
 module.exports.action = function (api, message, callback) {
-	var mentioned = message.body.split(module.exports.matchPattern)[1];
+	var mentioned = message.body.split(module.exports.matchPattern)[1].split(" ").slice(0,2).join(" ");
 	api.getUserID(mentioned, function (err, ids) {
 		if (err) {
 			console.log(err);
@@ -14,7 +14,6 @@ module.exports.action = function (api, message, callback) {
 					return setImmediate(callback);
 				}
 				else {
-
 					messageText = "This the HMS facebook bot. You have been mentioned by " + info[Object.getOwnPropertyNames(info)[0]].name + " in the group chat " + message.threadName + ". Here was the message: '" + message.body + "' \n\n If you believe this was done in error, please ignore this.";
 					api.sendMessage(messageText, validID['userID']);
 					setImmediate(callback);
