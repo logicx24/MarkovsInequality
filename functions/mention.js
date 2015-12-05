d9c920c7a744b7d176f3d94c7401b3f9f7b7667f
Aakash Japi <aakashjapi@gmail.com>
2015-12-04 18:30:22 -0800

    set up multiple mentions

--- functions/mention.js
+++ functions/mention.js
@@ -1,24 +1,28 @@
+async = require("async");
+
 module.exports.matchPattern = /@/;
-//@name lastname must be how you mention someone. No multiple mentions in a single comment (yet)
-module.exports.action = function (api, message, callback) {
-	var mentioned = message.body.split(module.exports.matchPattern)[1].split(" ").slice(0,2).join(" ");
-	api.getUserID(mentioned, function (err, ids) {
-		if (err) {
-			console.log(err);
-			return setImmediate(callback);
-		} else {
-			var validID = ids[0];
-			api.getUserInfo(message['senderID'], function (err, info) {
-				if (err) {
-					console.log(err);
-					return setImmediate(callback);
-				}
-				else {
-					messageText = "This the HMS facebook bot. You have been mentioned by " + info[Object.getOwnPropertyNames(info)[0]].name + " in the group chat " + message.threadName + ". Here was the message: '" + message.body + "' \n\n If you believe this was done in error, please ignore this.";
-					api.sendMessage(messageText, validID['userID']);
-					setImmediate(callback);
-				}
-			});
-		}
-	});
+//@name lastname must be how you mention someone. Multiple mentions also work.
+module.exports.action = function (api, message, cb) {
+	async.eachSeries(message.body.split(module.exports.matchPattern).slice(1), function (frag, callback) {
+		mentioned = frag.split(" ").slice(0,2).join(" ");
+		api.getUserID(mentioned, function (err, ids) {
+			if (err) {
+				console.log(err);
+				return setImmediate(callback);
+			} else {
+				var validID = ids[0];
+				api.getUserInfo(message['senderID'], function (err, info) {
+					if (err) {
+						console.log(err);
+						return setImmediate(callback);
+					}
+					else {
+						messageText = "This the HMS facebook bot. You have been mentioned by " + info[Object.getOwnPropertyNames(info)[0]].name + " in the group chat " + message.threadName + ". Here was the message: '" + message.body + "' \n\n If you believe this was done in error, please ignore this.";
+						api.sendMessage(messageText, validID['userID']);
+						setImmediate(callback);
+					}
+				});
+			}
+		});
+	}, cb);
 }
