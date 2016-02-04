4352f063e1c21a3079e6abbd67d0f5fb2fc35e3a
Aakash Japi <aakashjapi@gmail.com>
2016-02-04 15:29:01 -0800

    got rid of broken listeners implementation for now

--- listeners/index.js
+++
@@ -1,6 +0,0 @@
-require("fs").readdirSync(__dirname + "/").forEach(function (filename) {
-	if (filename.match(/\.js$/) && filename != "index.js") {
-		var fname = filename.replace(".js", "");
-		module.exports[fname] = require("./" + filename);
-	}
-});
--- listeners/test.js
+++
@@ -1,8 +0,0 @@
-var events = require("events");
-var eventEmitter = new events.EventEmitter();
-
-module.exports.eventName = "test";
-
-module.exports.shouldEmit = function () {
-	eventEmitter.emit(module.exports.eventName, {"sendMessage": "Test event emitted if subscribed to."});
-}
